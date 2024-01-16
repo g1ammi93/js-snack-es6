e6c916c3723256e771a0d0036e285d626a24ab57
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-16 19:20:54 +0000

    Added filter Snack 2

--- js/style.js
+++ js/style.js
@@ -17,10 +17,10 @@ const guests = [
 
 // Creo il nuovo Array
 
-const guestList = guests.map((guest, index) => ({
+const guestList = guests.map((guest, i) => ({
     tableName,
     guestName: guest,
-    seatNumber: index + 1
+    seatNumber: i + 1
 }))
 
 // Stampo in console
@@ -30,3 +30,18 @@ console.log(guestList)
 
 // Snack 2
 
+const students = [
+    { id: 213, name: 'Marco della Rovere', totalVote: 78 },
+    { id: 110, name: 'Paola Cortellessa', totalVote: 96 },
+    { id: 250, name: 'Andrea Mantegna', totalVote: 48 },
+    { id: 145, name: 'Gaia Borromini', totalVote: 74 },
+    { id: 196, name: 'Luigi Grimaldello', totalVote: 68 },
+    { id: 102, name: 'Piero della Francesca', totalVote: 50 },
+    { id: 120, name: 'Francesca da Polenta', totalVote: 84 }
+];
+
+const studentVoteOver70 = students.filter(student => student.totalVote > 70);
+console.log(studentVoteOver70)
+
+const studentVoteOver70AndOverId120 = students.filter((student) => student.totalVote > 70 && student.id > 120)
+console.log(studentVoteOver70AndOverId120)
