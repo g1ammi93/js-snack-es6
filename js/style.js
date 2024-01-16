5e7c02ba573fc44cecc90742fd0d24fdd126f316
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-16 19:34:31 +0000

    Added point 3 and print in page

--- js/style.js
+++ js/style.js
@@ -30,6 +30,8 @@ console.log(guestList)
 
 // Snack 2
 
+
+// Creo l'array con tutti i dati degli studenti
 const students = [
     { id: 213, name: 'Marco della Rovere', totalVote: 78 },
     { id: 110, name: 'Paola Cortellessa', totalVote: 96 },
@@ -40,8 +42,27 @@ const students = [
     { id: 120, name: 'Francesca da Polenta', totalVote: 84 }
 ];
 
+// FIltro gli studenti con voto totale superiore al 70
+
 const studentVoteOver70 = students.filter(student => student.totalVote > 70);
 console.log(studentVoteOver70)
 
+// Ora filtro gli studenti con voto superiore al 70 ed id superiore al 120
+
 const studentVoteOver70AndOverId120 = students.filter((student) => student.totalVote > 70 && student.id > 120)
 console.log(studentVoteOver70AndOverId120)
+
+// Creo una targa con l'elenco degli studenti e la stampo in pagina
+
+const studentTarget = document.getElementById('students')
+
+const nameUpperCase = students.map(students => students.name.toUpperCase())
+console.log(nameUpperCase)
+
+
+nameUpperCase.forEach(name => {
+    const list = document.createElement('li');
+    list.textContent = name;
+    studentTarget.appendChild(list)
+})
+
